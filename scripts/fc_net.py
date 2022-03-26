a164a785d1455c76328ad5b4a049fd7ce38ac706
lucyzmf <67420952+lucyzmf@users.noreply.github.com>
2022-03-26 23:43:26 +0100

    learning within inference call every 20 inference steps, 1 weight update

--- scripts/fc_net.py
+++ scripts/fc_net.py
@@ -154,6 +154,9 @@ class FcDHPC(nn.Module):
             r_act[-1], r_out[-1] = layers[-1](torch.matmul(torch.transpose(layers[-2].weights, 0, 1), e_act[-2]),
                                               r_act[-1])
 
+            if i > 0 and i % 20 == 0:
+                self.learn()
+
     def learn(self):
         # iterate through all non last layers to update weights
         for i in range(len(self.architecture) - 1):

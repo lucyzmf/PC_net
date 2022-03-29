cfcf4ddc20bfd97956ae40f8b1fe5bc810157e17
lucyzmf <67420952+lucyzmf@users.noreply.github.com>
2022-03-29 10:35:40 +0200

    cnn trained with smaller dataset and tested on still img

--- scripts/cnn.py
+++ scripts/cnn.py
@@ -142,8 +142,8 @@ optimizer = torch.optim.SGD(model.parameters(), lr=wbconfig.learning_rate)
 total_step = len(train_loader)
 for epoch in range(wbconfig.num_epochs):
     for i, (images, labels) in enumerate(train_loader):
-        # images = torch.unsqueeze(images, dim=1).to(device)
-        images = images.to(device)
+        images = torch.unsqueeze(images, dim=1).to(device)
+        # images = images.to(device)
         labels = labels.to(device)
 
         # Forward pass
@@ -170,8 +170,8 @@ for epoch in range(wbconfig.num_epochs):
             correct = 0
             total = 0
             for images, labels in test_still_img_loader:
-                # images = torch.unsqueeze(images.float(), dim=1).to(device)
-                images = images.to(device)
+                images = torch.unsqueeze(images.float(), dim=1).to(device)
+                # images = images.to(device)
                 labels = labels.to(device)
                 outputs = model(images)
                 _, predicted = torch.max(outputs.data, 1)

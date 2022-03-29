45ea384dfc14aec1196ec60e2ae7ecb216cf3330
lucyzmf <67420952+lucyzmf@users.noreply.github.com>
2022-03-29 14:00:35 +0200

    seq img testing

--- scripts/fc_net.py
+++ scripts/fc_net.py
@@ -42,7 +42,7 @@ class FCLayer(nn.Module):
         nn.init.normal_(self.weights, 0, 0.5)  # normal distribution
         # nn.init.constant_(self.weights, 0.5)  # constant distribution
         self.weights = torch.clamp(self.weights, min=0)  # weights clamped to above 0
-        self.weights = self.weights / self.layer_size  # normalise weights given next layer size
+        self.weights = self.weights / self.layer_size  # normalise weights given this layer size
 
     # def reset_state(self):
     # reinitialise activation and output values

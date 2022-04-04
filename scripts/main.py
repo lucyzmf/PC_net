18a2b1592d54bcfe24182d1ebfa5a59cc85b0e23
lucyzmf <67420952+lucyzmf@users.noreply.github.com>
2022-04-04 14:28:41 +0200

    morph test 6 seq train but reset after each seq

--- scripts/main.py
+++ scripts/main.py
@@ -109,7 +109,7 @@ if __name__ == '__main__':
         dataWidth = 28 + 2*config['padding_size']
 
         # Hyperparameters for training logged with wandb
-        wandb.init(project="DHPC_morph", entity="lucyzmf")  # , mode='disabled')
+        wandb.init(project="DHPC_morph_test_6", entity="lucyzmf")  # , mode='disabled')
 
         wbconfig = wandb.config
         wbconfig.infstep = inference_steps

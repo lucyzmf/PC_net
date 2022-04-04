93d0b0a337b2861a2a3b99b947d9a81d67bca6ee
lucyzmf <67420952+lucyzmf@users.noreply.github.com>
2022-04-04 14:41:48 +0200

    morph test 6 seq train but reset after each seq

--- scripts/main.py
+++ scripts/main.py
@@ -295,7 +295,11 @@ if __name__ == '__main__':
                     net.init_states()
                     for i, (_image, _label) in enumerate(test_loader):  # generate high level rep using spin seq test dataset
                         net(_image, inference_steps, istrain=False)
-                        if (i + 1) % frame_per_seq == 0:  # at the end of each sequence
+                        if not reset_per_frame and ((i + 1) % frame_per_seq) == 0:  # at the end of each sequence
+                            seq_rep_test.append(net.states['r_activation'][-1].detach().cpu().numpy())  # rep recorded at end of each seq
+                            seq_label_test.append(_label)
+                            net.init_states()
+                        if reset_per_frame:
                             seq_rep_test.append(net.states['r_activation'][-1].detach().cpu().numpy())  # rep recorded at end of each seq
                             seq_label_test.append(_label)
                             net.init_states()

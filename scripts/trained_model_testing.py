3251efcbd6511672a9fc1556971ab777827b51be
lucyzmf <67420952+lucyzmf@users.noreply.github.com>
2022-04-07 13:46:08 +0200

    state tensors transfer to cpu first

--- scripts/trained_model_testing.py
+++ scripts/trained_model_testing.py
@@ -149,9 +149,9 @@ for i, (_image, _label) in enumerate(test_loader_spin):
         for l in range(len(trained_net.architecture)):
             is_train.append(0)
             layer.append(l)
-            r_act.append(trained_net.states['r_activation'][l].numpy())
-            r_out.append(trained_net.states['r_output'][l].numpy())
-            e_out.append(trained_net.states['error'][l].numpy())
+            r_act.append(trained_net.states['r_activation'][l].cpu().numpy())
+            r_out.append(trained_net.states['r_output'][l].cpu().numpy())
+            e_out.append(trained_net.states['error'][l].cpu().numpy())
         trained_net.init_states()
 
 df_reps = pd.DataFrame()

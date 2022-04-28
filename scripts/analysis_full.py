ff4a904dc1cc52d6e8fcc7122b5e2c7444dcb286
lucyzmf <67420952+lucyzmf@users.noreply.github.com>
2022-04-28 14:24:32 +0200

    run trained model testing on lisa

---
+++ scripts/analysis_full.py
@@ -0,0 +1,19 @@
+'''
+inputs: data dir, dir of trained models
+
+baseline on imgs:
+    clustering of train img
+    clustering of train seq
+    gen:
+    train img to test img
+    train seq to test seq
+
+generate representations:
+    for each seq train model,
+        seq rep
+        frame rep
+        still rep (which should be subset of above)
+    for still trained model,
+        still rep 
+
+'''

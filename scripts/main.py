851b1f61cb671db6dec76a949531ebbff1f8bce1
lucyzmf <67420952+lucyzmf@users.noreply.github.com>
2022-05-26 12:26:39 +0200

    add more test to corr and gen acc

--- scripts/main.py
+++ scripts/main.py
@@ -75,7 +75,7 @@ if __name__ == '__main__':
     reset_per_frame = config['reset_per_frame']
 
     # set up test name
-    test_name = str(config['train_size']) + 'datasetsize_10classgenacc' + str(reset_per_frame) + '_seqtrain' + str(seq_train)
+    test_name = str(config['train_size']) + '_datasetsize_10classgenacc' + str(reset_per_frame) + '_seqtrain' + str(seq_train)
     print(test_name)
 
 
